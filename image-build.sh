6a7888718ff88c2732f641fca23c0b862296d053
Ramonog9 <ramonog9@gmail.com>
2025-09-29 17:09:08 -0500

    More yaml files

--- image-build.sh
+++ image-build.sh
@@ -4,7 +4,6 @@ set -e
 PROFILE_NAME=${1:-cloud-demo}
 
 eval $(minikube docker-env -p ${PROFILE_NAME})
-docker build -t cloud-demo:local ./app
-
+docker build -t cloud-demo:local .
 
 echo "Image 'cloud-demo:local' built inside Minikube."
