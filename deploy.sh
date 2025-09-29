ce6d471f3e241ddda31f54a95356ad501010a198
Ramonog9 <ramonog9@gmail.com>
2025-09-29 16:05:36 -0500

    Adding message to deploy bash file

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ set -e
 kubectl apply -f k8s/namespace.yaml
 kubectl apply -n cloud-demo -f k8s/deployment.yaml
 kubectl apply -n cloud-demo -f k8s/service.yaml
+
+echo "Successfully deployed cloud-demo"
